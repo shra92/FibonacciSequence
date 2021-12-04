69aa2a220a949c0a797814fd0eaca2c8132d67ef
Shra <shravanthibc@gmail.com>
2021-12-04 13:49:48 +0530

    Handling few errors

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,19 +1,22 @@
 def getFibonacci(input):
-    input = int(input)
-    if  input > 0:
-        first = 0
-        second = 1
-        sum = first + second;  #0,1,1,2,3,5,8,13
-        count = 1
-        print(first); print(second)
-       
-        while count < input-2:
-            count = count+1
-            first = second
-            second = sum
-            sum = first + second
-            print(sum)
-            
     
+    if input.isnumeric():
+        input = int(input)
+        if  input > 0:
+            first = 0
+            second = 1
+            sum = first + second;  #0,1,1,2,3,5,8,13
+            count = 1
+            print(first); print(second)
+        
+            while count < input-2:
+                count = count+1
+                first = second
+                second = sum
+                sum = first + second
+                print(sum)
+        else: print("Number must be greater than 0")       
+    else: print ("Please enter only a number greater than 0")
+
 number = input("Enter the number of Fibonacci series needed ")
 getFibonacci(number)
